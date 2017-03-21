33c66830ba68222f6463b7b38f3b6fd77e3f944b
joeyappelhans <9jappelhans@stpatrick.org>
2017-03-21 13:58:28 -0500

    started samecolor

---
+++ click.js
@@ -0,0 +1,16 @@
+//declare samecolor() here
+function samecolor(){
+    const doc = document;
+    let div = doc.querySelectorAll("#samecolor div");
+    div[0].style.backgroundColor=green;
+    div[1].style.backgroundColor=green;
+    div[2].style.backgroundColor=green;
+    div[3].style.backgroundColor=green;
+}
+
+//declare diffcolor() here
+function diffcolor(hex){
+    const doc = document;
+    let div= doc.querySelectorAll("#diffcolor");
+    div.style.backgroundColor=
+}
